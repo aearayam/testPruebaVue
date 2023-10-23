ef75bdd82060e71be1cdc2b9cdc2c9b9493153d0
aearayam <alexarayam4@gmail.com>
2023-10-23 14:50:16 -0300

    Eliminado div adsad

--- src/components/Tarea.vue
+++ src/components/Tarea.vue
@@ -17,9 +17,6 @@
                             <div v-if="loading" class="spinner-border text-success" role="status">
                                 <span class="visually-hidden">Loading...</span>
                             </div>
-                            <div>
-                                adassddsdsads
-                            </div>
                         </div>
                         <h5 v-if="listTareas.length == 0">No hay tareas por hacer.</h5>
                         <ul v-if="!loading" class="list-group">
